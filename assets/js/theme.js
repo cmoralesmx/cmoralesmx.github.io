6fee1c973618024b95a290f34e8c0edce744b3b9
Carlos Morales <carlosmorales777@gmail.com>
2020-12-07 22:02:46 +0000

    Add personal content
    This is a combination of 12 commits.
    
    1. Setup parameters for my site and add assets
    2. Update the front page
    3. Add my publications
    4. Add my CV
    5. Add my first entry to the blog
    6. Update about page
    7. Set initial configuration and basic info
    8. Add the rest of the initial content
    9. Change settings for deployment as user
    10. Add my google-analytics code
    11. Keep dark mode enabled & switchable, not default
    12. Use a size-optimized portrait

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -13,5 +13,10 @@ let initTheme = (theme) => {
 
   localStorage.setItem("theme", theme);
 }
-
+// The value stored in `theme` is null only on the first run. This triggers the
+// black theme being set as the default. Later, the value stored in `theme`
+// will be "null" as a result of the user switching off the dark mode
+if(localStorage.getItem("theme") === null){
+  localStorage.setItem("theme", "null");
+}
 initTheme(localStorage.getItem("theme"));
